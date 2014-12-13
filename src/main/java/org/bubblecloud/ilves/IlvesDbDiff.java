1cea69a3e6d1b22807af70139c113fccd142d690
Tommi S.E. Laukkanen <tommi.s.e.laukkanen@gmail.com>
2014-12-13 18:50:19 +0200

    Added main class fir easily generating liquibase change log entries according to JPA model changes.

---
+++ src/main/java/org/bubblecloud/ilves/IlvesDbDiff.java
@@ -0,0 +1,24 @@
+package org.bubblecloud.ilves;
+
+import org.apache.log4j.xml.DOMConfigurator;
+import org.vaadin.addons.sitekit.util.PersistenceUtil;
+import org.vaadin.addons.sitekit.util.PropertiesUtil;
+
+/**
+ * Created by tlaukkan on 12/13/2014.
+ */
+public class IlvesDbDiff {
+
+    /**
+     *
+     * @param args
+     */
+    public static void main(final String[] args) throws Exception {
+        // Configure logging.
+        DOMConfigurator.configure("log4j.xml");
+
+        PropertiesUtil.setCategoryRedirection("site", IlvesMain.PROPERTIES_FILE_PREFIX);
+        final String diff = PersistenceUtil.diff(IlvesMain.PERSISTENCE_UNIT, IlvesMain.PROPERTIES_FILE_PREFIX);
+        System.out.println(diff);
+    }
+}
